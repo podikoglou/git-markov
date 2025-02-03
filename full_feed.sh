eef1715b58aa66be6432178ba2710afbe8f830e2
Alex <alex.podikoglou@gmail.com>
2025-02-03 23:00:42 +0200

    feat: full_feed

---
+++ full_feed.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+ffeed() {
+  query=$1
+  model=$2
+
+  for order in 2 3; do
+
+    echo "[INFO] Feeding $model, order = $order"
+
+    path=models/order-$order/$model 
+
+    mkdir -p models/order-$order
+
+    ./feed.sh $query $path $order
+    bzip2 $path
+  done
+}
+
+ffeed '*.html' html.bc
+ffeed '*.md'   markdown.bc
+ffeed '*.js'   javascript.bc
+ffeed '*.ts'   typescript.bc
+ffeed '*.rs'   rust.bc
+ffeed '*.go'   go.bc
+ffeed '*.py'   python.bc
+ffeed '*.sh'   sh.bc
