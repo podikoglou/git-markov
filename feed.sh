185b1f50fb181108c7e2184f5edeb1b236259797
Alex <alex.podikoglou@gmail.com>
2025-02-03 22:15:38 +0200

    fix: args in feed script

--- feed.sh
+++ feed.sh
@@ -4,9 +4,9 @@ query=$1
 model=$2
 
 feed_markov() {
-  markov/target/release/markov train $model < $1
+  markov/target/release/markov train $1 < $2
 }
 
 export -f feed_markov
 
-find ./repos -name $query -exec bash -c 'feed_markov {}' \;
+find ./repos -name $query -exec bash -c "feed_markov $model {}" \;
