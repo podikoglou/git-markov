64d886e4c25d8a245c08b71da691ad8a76b5e9c5
Alex <alex.podikoglou@gmail.com>
2025-02-03 22:10:35 +0200

    feat: make feed.sh customizable

--- feed.sh
+++ feed.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+query=$1
+model=$2
+
 feed_markov() {
-  markov/target/release/markov train model.bc < $1
+  markov/target/release/markov train $model < $1
 }
 
 export -f feed_markov
 
-find ./repos -name '*.rs' -exec bash -c 'feed_markov {}' \;
+find ./repos -name $query -exec bash -c 'feed_markov {}' \;
